677f42974d66a2898c981d539870688f2e380826
Phat Truong Quang <tqphat31082k3@gmail.com>
2025-03-24 01:51:18 +0700

    first commit

--- app/src/main/java/com/example/qlsach/MainActivity.java
+++ app/src/main/java/com/example/qlsach/MainActivity.java
@@ -1,24 +1,82 @@
 package com.example.qlsach;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.qlsach.R;
+import com.google.android.material.navigation.NavigationView;
+
+// MainActivity.java
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Khởi tạo toolbar và navigation drawer
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Mặc định hiển thị fragment Dashboard
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new DashboardFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_dashboard);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Xử lý chuyển đổi giữa các fragment khi chọn menu
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dashboard) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new DashboardFragment()).commit();
+        } else if (id == R.id.nav_books) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new BooksFragment()).commit();
+        } else if (id == R.id.nav_authors) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AuthorsFragment()).commit();
+        } else if (id == R.id.nav_genres) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new GenresFragment()).commit();
+        } else if (id == R.id.nav_borrowings) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new BorrowingsFragment()).commit();
+        }
+
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
